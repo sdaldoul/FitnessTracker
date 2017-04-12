c0ceffdee74add8339264febc7f244ee19c03806
Slaheddine DALDOUL <daldoul.slaheddine@gmail.com>
2017-04-12 23:07:52 +0200

    Fix jre 7 issue

--- src/main/java/com/pluralsight/controller/HelloController.java
+++ src/main/java/com/pluralsight/controller/HelloController.java
@@ -7,11 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-	@RequestMapping(value= "/greeting")
-	public String sayHello(Model model) {
-
+	@RequestMapping(value ="/greeting")
+	public String sayHello (Model model) {
+		
 		model.addAttribute("greeting", "Hello World");
-		return "Hello";
+		
+		return "hello";
 	}
-
+	
 }
